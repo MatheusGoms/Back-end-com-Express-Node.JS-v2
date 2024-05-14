5f5ef95efb64627cb71d08f31cf5a21d62b638aa
Matheus <matheus.ag9610@gmail.com>
2024-05-14 18:48:07 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import manipulador404 from "./middlewares/manipulador404.js";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+app.use(manipulador404);
+
+app.use(manipuladorDeErros);
+
+
+
+export default app;
