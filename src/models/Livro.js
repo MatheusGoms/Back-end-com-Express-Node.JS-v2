34e4549e6540a2454cb6fab4faae9a9cca600140
Matheus <43159599+MatheusGoms@users.noreply.github.com>
2024-05-21 07:59:29 -0300

    Update Livro.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,7 +16,7 @@ const livroSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A editora é obrigatória'],
       enum: {
-        values: ['Casa do código', 'Alura'],
+        values: ['Netflix', 'Classicos'],
         message: 'A editora {VALUE} não é um valor permitido.'
       }
     },
